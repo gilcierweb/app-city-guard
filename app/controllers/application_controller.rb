09453c62e60fd09bdb5fab1a92fe6e935174fc5c
gilcier <gilcier.sousa@alfaconcursos.com.br>
2023-04-01 04:18:48 -0300

    improve form devise style

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base
+  layout :layout_by_resource
+
+  def after_sign_in_path_for(resource)
+    admin_root_path
+  end
+
+  private
+
+  def layout_by_resource
+    if devise_controller?
+      "auth"
+    else
+      "application"
+    end
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,34 @@
 module ApplicationHelper
+
+  def flash_class(level)
+    case level
+    when 'info' then
+      "alert alert-info"
+    when 'notice', 'success' then
+      "alert alert-success"
+    when 'error' then
+      "alert alert-danger"
+    when 'alert' then
+      "alert alert-warning"
+    end
+  end
+
+  def display_boolean(level)
+    case level
+    when 1, true, 'active' then
+      'Ativo'
+    when 0, false, 'inactive' then
+      'Inativo'
+    end
+  end
+
+  def display_boolean_raw(level)
+    case level
+    when 1, true, 'active', 'yes', 'yes_ap' then
+      'Sim'
+    when 0, false, 'inactive', 'no', 'no_ap' then
+      'Não'
+    end
+  end
+
 end
