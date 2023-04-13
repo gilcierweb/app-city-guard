43def2985a31fa7301b0250f670f3bb68ebd30e6
gilcier <gilcier.sousa@alfaconcursos.com.br>
2023-04-13 19:15:02 -0300

    addin model and views admin

--- app/javascript/admin_load.js
+++ app/javascript/admin_load.js
@@ -1,7 +1,28 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 
-import "./admin/core/libs.min"
+// "js": {
+//     "core": [
+//         "./node_modules/jquery/dist/jquery.js",
+//         "./node_modules/waypoints/lib/noframework.waypoints.min.js",
+//         "./node_modules/datatables.net/js/jquery.dataTables.min.js",
+//         "./node_modules/@popperjs/core/dist/umd/popper.min.js",
+//         "./node_modules/bootstrap/dist/js/bootstrap.min.js",
+//         "./node_modules/datatables.net-bs5/js/dataTables.bootstrap5.min.js",
+//         "./{directory}/assets/vendor/lodash/lodash.min.js",
+//         "./node_modules/swiper/swiper-bundle.min.js"
+//     ],
+//         "external": [
+//         "./node_modules/nouislider/distribute/nouislider.min.js",
+//         "./node_modules/vanillajs-datepicker/dist/js/datepicker-full.js",
+//         "./node_modules/smooth-scrollbar/dist/smooth-scrollbar.js",
+//         "./{directory}/assets/js/plugins/circle-progress.js",
+//         "./node_modules/apexcharts/dist/apexcharts.min.js",
+//         "./node_modules/counterup2/dist/index.js",
+//         "./src/assets/vendor/Leaflet/leaflet.js"
+//     ]
+// }
+// import "./admin/core/libs.min"
 import "./admin/core/external.min"
 // import "./admin/plugins/circle-progress"
 import "./admin/plugins/setting"
